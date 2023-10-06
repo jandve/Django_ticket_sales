55276c4ee1dae6158534e221994488f0c5995b85
jorlopsim <140028642+jorlopsim@users.noreply.github.com>
2023-10-05 22:32:54 -0400

    Add Custom Products (#12)

--- ticket_sales/concerts/serializers/product.py
+++ ticket_sales/concerts/serializers/product.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from ..models.product import Product
-
+from .place import PlaceSerializer,GroupPlaceSerializer
+from django.http import JsonResponse
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model=Product
         fields="__all__"
+
+class ProductPlacesSerializer(serializers.Serializer):
+    ##place = GroupPlaceSerializer(many = True)
+    place=GroupPlaceSerializer()
+    count = serializers.IntegerField()
+    products = ProductSerializer(many = True)
